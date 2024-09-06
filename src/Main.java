1c29391e959aed1688901519d7eda21f4e20ec23
Pawel-Wasilewski <wasilewski.pawel2007@gmail.com>
2024-09-06 11:37:39 +0200

    dodanie dużych i małych liter

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,13 @@ public class Main {
 
         while (haslo.length() != 24) {
             int losowa_litera = (int) (Math.random() * litery_s.length());
-            haslo = haslo + litery_s.charAt(losowa_litera);
+            double duza_or_mala =  Math.random();
+
+            if (duza_or_mala >= 0.5) {
+                haslo = haslo + litery_d.charAt(losowa_litera);
+            } else {
+                haslo = haslo + litery_s.charAt(losowa_litera);
+            }
         }
         System.out.println(haslo);
     }
